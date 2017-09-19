e447759ad006cf71fe72780c1791a686a0c80dba
antonincouvert <antocoucou@gmail.com>
2017-09-19 16:39:07 +0200

    Cinquième version

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
 
-	require 'Modele.php';
-	
-	try
-	{
-		$billets = getBillets();
-		require 'vueAccueil.php';
+require('Controleur.php');
+
+try {
+	if (isset($_GET['action'])) {
+		if ($_GET['action'] == 'billet') {
+			if (isset($_GET['id'])) {
+				$idBillet = intval($_GET['id']);
+				if ($idBillet != 0)
+					billet($idBillet);
+					else
+						throw new Exception("Identifiant de billet non valide");
+			}
+			else
+				throw new Exception("Identifiant de billet non défini");
+		}
+		else
+			throw new Exception("Action non valide");
 	}
-	catch (Exception $e)
-	{
-		$msgErreur = $e->getMessage();
-		require 'vueErreur.php';
+	else {
+		accueil();  // action par défaut
 	}
+}
+catch (Exception $e) {
+	erreur($e->getMessage());
+}
+
 ?>
